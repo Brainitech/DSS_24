82c29452fa81eb61962e1296380b798e7e81ff49
Brainitech <gaming.brainiac2020@gmail.com>
2024-06-29 03:43:12 +0530

    Added Dropdown and custom cursor

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
       borderWidth: {
         3: "3px",
       },
+      center: true,
+      padding: "1rem",
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "1024px",
+        xl: "1280px",
+        lm: "1060px",
+      },
     },
   },
   plugins: [],
