cfb51d093487063f0ba425262d83c28aa8497f6e
Brainitech <gaming.brainiac2020@gmail.com>
2024-06-30 12:48:40 +0530

    Footer bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css"
 
 import CustomCursor from "./components/Cursor/CustomCursor.jsx"
 import Navbar from "./components/NavBar/navbar.jsx"
+import Footer from "./components/Footer/footer.jsx"
 
 function App() {
   return (
     <>
       <CustomCursor />
       <Navbar />
+      <Footer />
     </>
   )
 }
