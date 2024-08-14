73a9c793017b03ef1a3d39d7fd736dbb0557bacf
Brainitech <gaming.brainiac2020@gmail.com>
2024-08-15 02:16:46 +0530

    Speakers now take info dynamically

--- src/components/Speakers/speakers.jsx
+++ src/components/Speakers/speakers.jsx
@@ -1,5 +1,54 @@
-// import "../Speakers/speakers.css"
+import React from "react"
+import PropTypes from "prop-types"
 import Pic from "../../assets/me.png"
+
+export function Speakercard(props) {
+  const { pic, name, text } = props
+
+  const speakername = "text-white font-bold text-center text-2xl"
+  const speakercontent = "flex flex-col items-center justify-center sm:flex-row"
+  const speakertext = "text-xs sm:text-base sm:px-9 tracking-wide text-justify text-white flex sm:max-w-3xl"
+  const speakerpic = "rounded-2xl max-w-36 sm:max-h-60 max-h-40 sm:rounded-2xl"
+  const speakerbg = "flex bg-slate-700 rounded-3xl m-8 p-5 max-w-max"
+
+  const isAlternate = props.isAlternate
+
+  return (
+    <div className={speakerbg}>
+      <div className="m-2 justify-center items-center">
+        <div className={speakername}>{name}</div>
+        {isAlternate && (
+          <div className={speakercontent}>
+            <div className={`${speakertext} order-2 sm:order-1`}>
+              <p>{text}</p>
+            </div>
+            <div className="m-2 order-1 sm:order-2">
+              <img src={pic} alt="" className={speakerpic} />
+            </div>
+          </div>
+        )}
+
+        {!isAlternate && (
+          <div className={speakercontent}>
+            <div className="m-2">
+              <img src={pic} alt="" className={speakerpic} />
+            </div>
+            <div className={speakertext}>
+              <p>{text}</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+Speakercard.propTypes = {
+  pic: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  isAlternate: PropTypes.bool,
+}
 
 export default function Speakers() {
   return (
@@ -10,65 +59,24 @@ export default function Speakers() {
       </div>
       <div className="alumini1">
         <div className="flex flex-col items-center justify-center">
-          <div className="flex bg-slate-700 rounded-3xl m-8 p-5 max-w-max">
-            <div className="m-2 justify-center items-center">
-              <div className="text-white font-bold text-center text-2xl">Shikhar Goel</div>
-              <div className="flex flex-col items-center justify-center sm:flex-row">
-                <div className="m-2">
-                  <img src={Pic} alt="" className="rounded-2xl w-50 h-45 sm:max-w-36 sm:h-60 sm:rounded-2xl" />
-                </div>
-                <div className="text-xs sm:text-base sm:px-9 tracking-wide text-justify text-white flex sm:max-w-3xl">
-                  <p>
-                    Shikhar Goel is the CTO of GeeksforGeeks, a platform that provides computer science resources and coding challenges. He leads the technical team and develops innovative products
-                    and services for programmers and technology enthusiasts. He has launched GeeksforGeeks Courses, IDE, Practice, Mock Tests, and Articles. He is also a coder, a competitive
-                    programmer, a mentor, a speaker, and a community leader. He graduated from JIIT Noida in 2015 and joined GeeksforGeeks in 2016. He is based in Noida, India. He has been recognized
-                    as one of the top 10 young CTOs in India by GPL Awards in 2021. He is passionate about creating impactful products that can empower programmers and technology enthusiasts
-                    worldwide.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex bg-slate-700 rounded-3xl m-8 p-5 max-w-max">
-            <div className="m-2 justify-center items-center">
-              <div className="text-white font-bold text-center text-2xl">Shikhar Goel</div>
-              <div className="flex flex-col items-center justify-center sm:flex-row">
-                <div className="text-xs sm:text-base sm:px-9 tracking-wide text-justify text-white flex sm:max-w-3xl">
-                  <p>
-                    Shikhar Goel is the CTO of GeeksforGeeks, a platform that provides computer science resources and coding challenges. He leads the technical team and develops innovative products
-                    and services for programmers and technology enthusiasts. He has launched GeeksforGeeks Courses, IDE, Practice, Mock Tests, and Articles. He is also a coder, a competitive
-                    programmer, a mentor, a speaker, and a community leader. He graduated from JIIT Noida in 2015 and joined GeeksforGeeks in 2016. He is based in Noida, India. He has been recognized
-                    as one of the top 10 young CTOs in India by GPL Awards in 2021. He is passionate about creating impactful products that can empower programmers and technology enthusiasts
-                    worldwide.
-                  </p>
-                </div>
-                <div className="m-2">
-                  <img src={Pic} alt="" className="rounded-2xl w-50 h-45 sm:max-w-36 sm:h-60 sm:rounded-2xl" />
-                </div>
-              </div>
-            </div>
-          </div>
-          
-          <div className="flex bg-slate-700 rounded-3xl m-8 p-5 max-w-max">
-            <div className="m-2 justify-center items-center">
-              <div className="text-white font-bold text-center text-2xl">Shikhar Goel</div>
-              <div className="flex flex-col items-center justify-center sm:flex-row">
-                <div className="m-2">
-                  <img src={Pic} alt="" className="rounded-2xl w-50 h-45 sm:max-w-36 sm:h-60 sm:rounded-2xl" />
-                </div>
-                <div className="text-xs sm:text-base sm:px-9 tracking-wide text-justify text-white flex sm:max-w-3xl">
-                  <p>
-                    Shikhar Goel is the CTO of GeeksforGeeks, a platform that provides computer science resources and coding challenges. He leads the technical team and develops innovative products
-                    and services for programmers and technology enthusiasts. He has launched GeeksforGeeks Courses, IDE, Practice, Mock Tests, and Articles. He is also a coder, a competitive
-                    programmer, a mentor, a speaker, and a community leader. He graduated from JIIT Noida in 2015 and joined GeeksforGeeks in 2016. He is based in Noida, India. He has been recognized
-                    as one of the top 10 young CTOs in India by GPL Awards in 2021. He is passionate about creating impactful products that can empower programmers and technology enthusiasts
-                    worldwide.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Speakercard
+            pic={Pic}
+            name="Shikhar Goel"
+            text="Shikhar Goel is the CTO of GeeksforGeeks, a platform that provides computer science resources and coding challenges. He leads the technical team and develops innovative products and services for programmers and technology enthusiasts. He has launched GeeksforGeeks Courses, IDE, Practice, Mock Tests, and Articles. He is also a coder, a competitive programmer, a mentor, a speaker, and a community leader. He graduated from JIIT Noida in 2015 and joined GeeksforGeeks in 2016. He is based in Noida, India. He has been recognized as one of the top 10 young CTOs in India by GPL Awards in 2021. He is passionate about creating impactful products that can empower programmers and technology enthusiasts worldwide."
+            isAlternate={false}
+          />
+          <Speakercard
+            pic={Pic}
+            name="Shikhar Goel"
+            text="Shikhar Goel is the CTO of GeeksforGeeks, a platform that provides computer science resources and coding challenges. He leads the technical team and develops innovative products and services for programmers and technology enthusiasts. He has launched GeeksforGeeks Courses, IDE, Practice, Mock Tests, and Articles. He is also a coder, a competitive programmer, a mentor, a speaker, and a community leader. He graduated from JIIT Noida in 2015 and joined GeeksforGeeks in 2016. He is based in Noida, India. He has been recognized as one of the top 10 young CTOs in India by GPL Awards in 2021. He is passionate about creating impactful products that can empower programmers and technology enthusiasts worldwide."
+            isAlternate={true}
+          />
+          <Speakercard
+            pic={Pic}
+            name="Shikhar Goel"
+            text="Shikhar Goel is the CTO of GeeksforGeeks, a platform that provides computer science resources and coding challenges. He leads the technical team and develops innovative products and services for programmers and technology enthusiasts. He has launched GeeksforGeeks Courses, IDE, Practice, Mock Tests, and Articles. He is also a coder, a competitive programmer, a mentor, a speaker, and a community leader. He graduated from JIIT Noida in 2015 and joined GeeksforGeeks in 2016. He is based in Noida, India. He has been recognized as one of the top 10 young CTOs in India by GPL Awards in 2021. He is passionate about creating impactful products that can empower programmers and technology enthusiasts worldwide."
+            isAlternate={false}
+          />
         </div>
       </div>
     </div>
